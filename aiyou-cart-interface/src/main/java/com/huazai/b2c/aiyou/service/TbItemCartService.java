c9916579578bdd05ddf4744600c5ca0ecc2f1f29
panda <dl.herbert@foxmail.com>
2020-02-24 17:10:57 +0800

    新增购物车商品视图对象

---
+++ aiyou-cart-interface/src/main/java/com/huazai/b2c/aiyou/service/TbItemCartService.java
@@ -0,0 +1,20 @@
+package com.huazai.b2c.aiyou.service;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description 商品详情Controller
+ *              </ul>
+ * @className TbItemCartService
+ * @package com.huazai.b2c.aiyou.service
+ * @createdTime 2017年06月18日
+ *
+ * @version V1.0.0
+ */
+public class TbItemCartService
+{
+
+	
+}
